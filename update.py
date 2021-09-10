22f4e673cc590ebe1fe66b7803491cd695574c5b
tmkha <53520595+tmkha@users.noreply.github.com>
2021-09-10 21:44:21 +0700

    Update update.py

--- update.py
+++ update.py
@@ -1,1 +1,7 @@
-import requests
+import request, io
+
+def update_lib():
+	obb = requests.get('https://raw.githubusercontent.com/tmkha/splinterlands/main/splib.py')
+	saveFile('splib.py', obb)
+	data_team = requests.get('https://raw.githubusercontent.com/tmkha/splinterlands/main/team.json')
+	saveFile('../team.json')
