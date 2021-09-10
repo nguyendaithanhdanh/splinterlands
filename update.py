83bbb167849c944869b5e796d89bbdcf33830fb1
tmkha <53520595+tmkha@users.noreply.github.com>
2021-09-10 22:02:20 +0700

    Update update.py

--- update.py
+++ update.py
@@ -1,7 +1,10 @@
-import request, io
+import requests, io, os, shutil
 
 def update_lib():
 	obb = requests.get('https://raw.githubusercontent.com/tmkha/splinterlands/main/splib.py')
-	saveFile('splib.py', obb)
+	saveFile('/data/lib/splib.py', obb)
 	data_team = requests.get('https://raw.githubusercontent.com/tmkha/splinterlands/main/team.json')
 	saveFile('../team.json')
+	os.remove('splib.py')
+	shutil.rmtree('__pycache__') 
+	os.remove('update.py')
