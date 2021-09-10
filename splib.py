ad116fed3c0a29f60f46655edb7c05046446210a
tmkha <53520595+tmkha@users.noreply.github.com>
2021-09-10 12:03:22 +0700

    Update splib.py

--- splib.py
+++ splib.py
@@ -265,8 +265,9 @@ def shutDown(mess):
 
 def update():
 	response = requests.get('https://raw.githubusercontent.com/tmkha/splinterlands/main/splib.py')
-	with open('splib.py', mode="w", encoding="utf-8") as f:
-		f.write(response.content)
+	f = io.open('splib.py', mode="w", encoding="utf-8"):
+	f.write(response.content)
+	f.close()
 
 def check_update():
 	global version
