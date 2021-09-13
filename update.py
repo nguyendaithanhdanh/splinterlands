557ac564b3e4b85e3d6cf2d4bf514fda32cd7de2
tmkha <53520595+tmkha@users.noreply.github.com>
2021-09-13 22:28:35 +0700

    Update update.py

--- update.py
+++ update.py
@@ -1,10 +1,16 @@
-import requests, io
+import requests, io, os, shutil
 
+#Update for 1.2
 def saveFile(filePath, content):
 	f = io.open(filePath, mode="w", encoding="utf-8")
 	f.write(content)
 	f.close()
 
 def update_lib():
+
 	obb = requests.get('https://raw.githubusercontent.com/tmkha/splinterlands/main/splib.py')
 	saveFile('splib.py', obb.text)
+	try:
+		import PyQt5
+	except Exception as e:
+		os.system('pip install PyQt5')
