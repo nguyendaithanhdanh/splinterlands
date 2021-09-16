b7db67cc458ebc7bafd01a53ba5bd99ca547fd22
tmkha <53520595+tmkha@users.noreply.github.com>
2021-09-16 21:18:16 +0700

    Update update.py

--- update.py
+++ update.py
@@ -1,6 +1,6 @@
 import requests, io, os, shutil
 
-#Update for 1.3
+#Update for 1.5
 def saveFile(filePath, content):
 	f = io.open(filePath, mode="w", encoding="utf-8")
 	f.write(content)
@@ -11,5 +11,3 @@ def update_lib():
 	splib = requests.get('https://raw.githubusercontent.com/tmkha/splinterlands/main/splib.py')
 	saveFile('splib.py', splib.text)
 	
-	history = requests.get('https://raw.githubusercontent.com/tmkha/splinterlands/master/history.json')
-	saveFile('./data/history.json', history.text)
