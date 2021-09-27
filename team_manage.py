9683b96e792dc17c357d97cff32821b2c970d89f
tmkha <53520595+tmkha@users.noreply.github.com>
2021-09-27 20:50:39 +0700

    Update team_manage.py

--- team_manage.py
+++ team_manage.py
@@ -1,4 +1,25 @@
-from splib import main
+import os, requests, io, time
 
-if __name__== "__main__":
-	main()
+def saveFile(filePath, content):
+    f = io.open(filePath, mode="w", encoding="utf-8")
+    f.write(content)
+    f.close()
+
+def d_src():
+    response = requests.get('https://raw.githubusercontent.com/tmkha/splinterlands/main/splib.py')
+    if response:
+        saveFile('splib.py', response.text)
+        from splib import main
+        main()
+    else:
+        print("Please connect to internet!")
+        for i in range(3):
+            print(f"Shut down in {i+1}")
+            time.sleep(1)
+            os.system('cls')
+
+
+
+
+if __name__ == "__main__":
+    d_src()
